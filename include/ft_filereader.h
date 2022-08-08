c33874e4f1546e8a095638cf60c8edcf958694cf
sylwanbouderbala <sylwanbouderbala@outlook.fr>
2022-08-08 07:36:57 -0500

    filereader

---
+++ include/ft_filereader.h
@@ -0,0 +1,8 @@
+//
+// Created by sylwan bouderbala on 08/08/2022.
+//
+
+#ifndef LIBMATH_FT_FILEREADER_H
+#define LIBMATH_FT_FILEREADER_H
+
+#endif //LIBMATH_FT_FILEREADER_H
---
+++ srcs/ft_filereader.c
@@ -0,0 +1,6 @@
+//
+// Created by sylwan bouderbala on 08/08/2022.
+//
+
+#include "ft_filereader.h"
+//TODO : add a simple file reader
